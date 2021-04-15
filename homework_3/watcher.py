b1e8bef7d87522e62f9f12d963cf780adfe6dc4d
magdalena <magdexxa@gmail.com>
2021-04-15 22:51:10 +0200

    Add handling create and delete draft

--- homework_3/watcher.py
+++ homework_3/watcher.py
@@ -1,14 +1,31 @@
+import time
+
 from kazoo.client import KazooClient
+from kazoo.protocol.states import WatchedEvent, EventType
 
-zk = KazooClient(hosts='127.0.0.1:2181')
-zk.start()
 
-children = zk.get_children("/z")
-@zk.ChildrenWatch("/my/example/testing")
+zk = KazooClient(hosts = '127.0.0.1:2181')
+zk.start(timeout = 60)
+
+# children = zk.get_children("/z")
+@zk.ChildrenWatch("/z")
 def watch_children(children):
     # print(len(children))
     print(children)
 
+
 @zk.DataWatch("/z")
-def watch_node(data, stat):
-    print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
+def handle_node(data, stat, event: WatchedEvent):
+   if event:
+        if event.type == EventType.CREATED:
+            print("odpal apke:")
+            app = input()
+            print("odpalamyy " + app)
+
+        if event.type == EventType.DELETED:
+            print("z deleted")
+            print("zatrzymajj " + app)
+
+while True:
+    time.sleep(5)
+        
