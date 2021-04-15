bb1274d8fca421fc293e25d71831326f37a87407
magdalena <magdexxa@gmail.com>
2021-04-16 01:16:34 +0200

    Tiny progress maybe

--- homework_3/watcher.py
+++ homework_3/watcher.py
@@ -11,11 +11,14 @@ zk = KazooClient(hosts = '127.0.0.1:2181')
 zk.start(timeout = 60)
 app = None
 
-# children = zk.get_children("/z")
-@zk.ChildrenWatch("/z")
-def watch_children(children):
-    # print(len(children))
-    print(children)
+# # children = zk.get_children("/z")
+# @zk.ChildrenWatch("/z")
+# def watch_children(children):
+#     # for child in children:
+#     #     grandkids = zk.get_children("/z/" + child)
+#     #     print(grandkids)
+#     print(len(children))
+#     # print(children)
 
 
 @zk.DataWatch("/z")
@@ -24,7 +27,7 @@ def handle_node(data, stat, event: WatchedEvent):
     if event:
         if event.type == EventType.CREATED:
             print("To launch Discord press 1\nTo launch LOCALHOST game press 2\
-                \nTo specify app to launch press 3: ")
+                \nTo specify app to launch press 3: \nDismiss press 4")
             option = input()
             if int(option) == 1:
                 app = subprocess.Popen(["/home/magdalena/Discord/Discord"])
@@ -34,14 +37,20 @@ def handle_node(data, stat, event: WatchedEvent):
                 print("App to launch (with full path): ")
                 app_path = input()
                 app = subprocess.Popen([app_path])
-            
-            print(app.pid)
+            if int(option) == 4:
+                pass
+            else:
+                pass
 
 
         if event.type == EventType.DELETED:
             print("z deleted")
             if app != None:
                 os.kill(app.pid, signal.SIGKILL)
+
+
+    if zk.exists("z"):
+        print("Exists")
             
 while True:
     time.sleep(5)
