b1853a347d4fa17d7a72f0422c10d009d806e5b1
magdalena <magdexxa@gmail.com>
2021-05-17 01:14:38 +0200

    Remove artist in lyrics

--- homework_5/lyrics.py
+++ homework_5/lyrics.py
@@ -4,8 +4,6 @@ import requests
 
 song = "self control"
 song = song.replace(' ', '%20')
-author = 'frank ocean'
-author = author.replace(' ', '%20')
 song_url = 'https://some-random-api.ml/lyrics?title=' + song
 panda_url = 'https://some-random-api.ml/facts/panda'
 #fact = json.loads(requests.get(panda_url).content)["fact"]
