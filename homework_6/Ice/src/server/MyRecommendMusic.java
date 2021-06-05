317540e78c2307628c988e8a9f289bf05b6d6cbf
magdalena <magdexxa@gmail.com>
2021-06-05 15:48:59 +0200

    Add getting panda fact in recommendMusic

--- homework_6/Ice/src/server/MyRecommendMusic.java
+++ homework_6/Ice/src/server/MyRecommendMusic.java
@@ -3,9 +3,12 @@ package server;
 import Hello.IRecommendMusic;
 import com.zeroc.Ice.Current;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import org.json.*;
 
 public class MyRecommendMusic implements IRecommendMusic {
 
@@ -22,10 +25,25 @@ public class MyRecommendMusic implements IRecommendMusic {
     }
 
     @Override
-    public String recommendMusic(Current current) {
+    public String recommendMusic(Current current) throws IOException {
 
         Random random = new Random();
         int randomInt = random.nextInt(this.bands.size());
-        return bands.get(randomInt);
+//        return bands.get(randomInt);
+
+        URL url = new URL("https://some-random-api.ml/facts/panda");
+        JSONTokener tokener = new JSONTokener(url.openStream());
+        JSONObject obj = new JSONObject(tokener);
+        String fact = obj.getString("fact");
+        return fact;
     }
+
+//    public String getPandaFact(Current current) {
+//        String url = "https://some-random-api.ml/facts/panda";
+//        JSONObject jsonObject = new JSONObject(url);
+//        String fact = (String) jsonObject.getJSONObject(url).get("fact");
+//        return fact;
+//    }
+
+
 }
