88fd9ccc4ef729757a035692f80f51853960f5b8
magdalena <magdexxa@gmail.com>
2021-06-05 02:05:07 +0200

    Folder clean up

---
+++ homework_6/Ice/out/production/Ice/sr/ice/client/client.py
@@ -0,0 +1,20 @@
+import sys
+import Ice
+Ice.loadSlice('../../../../slice/hello.ice')
+import Hello
+
+
+if __name__ == '__main__':
+    with Ice.initialize(['--Ice.Config=config.client']) as communicator:
+        base = communicator.stringToProxy('recommendMusic' + ":tcp -h localhost -p 10000")
+        music_proxy = Hello.IRecommendMusicPrx.checkedCast(base)
+        if not music_proxy:
+            print("error")
+        else:
+            print("all good")
+
+        while True:
+            line = input()
+            if line == "music":
+                print(music_proxy.recommendMusic())
+
